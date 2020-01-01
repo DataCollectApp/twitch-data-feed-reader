0cd7103f05d05268565d3039b7907f005738b083
DataCollectApp <datacollectapp@jarand.dev>
2020-01-01 09:55:50 +0100

    #1: Added event mapping for punishment events

--- src/main/java/app/datacollect/twitchdata/feed/reader/EventMapper.java
+++ src/main/java/app/datacollect/twitchdata/feed/reader/EventMapper.java
@@ -5,6 +5,9 @@ import app.datacollect.twitchdata.feed.events.EventType;
 import app.datacollect.twitchdata.feed.events.ObjectType;
 import app.datacollect.twitchdata.feed.events.Version;
 import app.datacollect.twitchdata.feed.events.chatmessage.v1.ChatMessageEventV1;
+import app.datacollect.twitchdata.feed.events.clearchat.v1.ClearChatEventV1;
+import app.datacollect.twitchdata.feed.events.clearmessage.v1.ClearMessageEventV1;
+import app.datacollect.twitchdata.feed.events.globalclearchat.v1.GlobalClearChatEventV1;
 import org.json.JSONObject;
 
 class EventMapper {
@@ -14,6 +17,24 @@ class EventMapper {
       if (eventType == EventType.CHAT_MESSAGE_SNAPSHOT) {
         if (version == Version.V1) {
           return new ChatMessageEventV1(new JSONObject(content));
+        }
+      }
+    } else if (objectType == ObjectType.CLEAR_MESSAGE) {
+      if (eventType == EventType.CLEAR_MESSAGE_SNAPSHOT) {
+        if (version == Version.V1) {
+          return new ClearMessageEventV1(new JSONObject(content));
+        }
+      }
+    } else if (objectType == ObjectType.CLEAR_CHAT) {
+      if (eventType == EventType.CLEAR_CHAT_SNAPSHOT) {
+        if (version == Version.V1) {
+          return new ClearChatEventV1(new JSONObject(content));
+        }
+      }
+    } else if (objectType == ObjectType.GLOBAL_CLEAR_CHAT) {
+      if (eventType == EventType.GLOBAL_CLEAR_CHAT_SNAPSHOT) {
+        if (version == Version.V1) {
+          return new GlobalClearChatEventV1(new JSONObject(content));
         }
       }
     }
